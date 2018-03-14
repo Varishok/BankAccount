4446e32ebc6ef45fd693430901adf319820f1b09
Varishok <36737655+Varishok@users.noreply.github.com>
2018-03-14 11:32:43 +0400

    Update BankInformation.cs

--- BankInformation.cs
+++ BankInformation.cs
@@ -1,40 +1,61 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Collections;
 
-namespace BankAccount
+
+namespace Bank
 {
-    class BankInformation
+    public class Data
     {
-        private string name;
-        private int percent;
-
-        public BankInformation(string str, int per)
+        static string xmlFile = "userData.xml";
+        private DataSet dataSet = new DataSet();
+        
+        protected Data()
         {
-            name = str;
-            percent = per;
+            this.dataSet.ReadXml(xmlFile, XmlReadMode.InferSchema);
         }
-
-        public void SetName(string str)
+        static private List<string> userList = new List<string>
         {
-            name = str;
-        }
+            "vasya0102",
+            "dima1993",
+            "xkirilx",
+            "valera11",
+            "domava",
+            "arava",
+            "busy"
+        };
 
-        public void SetPercent(int per)
+        protected bool Authorization(string name, string pass, ref DataRow user, ref List<DataRow> account)
         {
-            percent = per;
-        }
-
-        public string GetName()
-        {
-            return name;
-        }
-
-        public int GetPercent()
-        {
-            return percent;
+            int user_id = userList.IndexOf(name);
+            if (user_id != -1)
+            {
+                int k = 0;
+                DataRow count;
+                user = dataSet.Tables[0].Rows[user_id];
+                for(int i = 0; i < user_id; i++)
+                {
+                    count = dataSet.Tables[0].Rows[i];
+                    k += int.Parse(count[4].ToString());
+                }
+                count = dataSet.Tables[0].Rows[user_id];
+                for(int i = 0; i < int.Parse(count[4].ToString()); i++)
+                {
+                    account.Add(dataSet.Tables[1].Rows[k]);
+                    k++;
+                }
+                var rows = dataSet.Tables[0].Rows[user_id];
+                if (pass == rows[1].ToString())
+                {
+                    Console.WriteLine(rows[0]);
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
