73779728fb1f975d67bb195249d0e9af472045a5
Varishok <36737655+Varishok@users.noreply.github.com>
2018-03-14 11:35:27 +0400

    Update Account.cs

--- Account.cs
+++ Account.cs
@@ -1,40 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BankAccount
+namespace Bank
 {
-    class Account
+    public class Account
     {
-        protected uint accountNumber;
-        protected int accountType;
-        protected string accountDate;
-        protected int accountSum;
-        protected BankInformation information;
-
-        public uint Number
-        {
-            get { return accountNumber; }
-            set { accountNumber = value; }
-        }
-
-        public int Type
-        {
-            set { accountType = value; }
-        }
-
-        public string Date
-        {
-            get { return accountDate; }
-            set { accountDate = value; }
-        }
-
-        public int Sum
-        {
-            get { return accountSum; }
-            set { accountSum = value; }
-        }
+        protected string nameAccount;
+        protected DateTime dateReg;
+        protected DateTime dateExp;
     }
 }
