5b0791f941ec9cfbfbdc36db7a72239dfceea7de
Varishok <36737655+Varishok@users.noreply.github.com>
2021-09-21 14:37:29 +0400

    Upload files / 2

---
+++ BankWPF/Information.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BankWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для Information.xaml
+    /// </summary>
+    public partial class Information : Window
+    {
+        public Information()
+        {
+            InitializeComponent();
+        }
+
+        private void AddDeposit(object sender, RoutedEventArgs e)
+        {
+            Adding deposit = new Adding(true);
+            this.Hide();
+            deposit.ShowDialog();
+            this.Show();
+        }
+
+        private void AddCredit(object sender, RoutedEventArgs e)
+        {
+            Adding credit = new Adding(false);
+            this.Hide();
+            credit.ShowDialog();
+            this.Show();
+        }
+    }
+}
---
+++ BankWPF/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BankWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Information win2 = new Information();
+            this.Hide();
+            win2.ShowDialog();
+            this.Close();
+        }
+
+        //void App_Startup(object sender, StartupEventArgs e)
+        //{
+        //    MainWindow firstWin = new MainWindow();
+        //    firstWin.Show();
+        //}
+    }
+}
